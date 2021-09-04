e1c37918a9a709a0f70485fcaf23683600fee5f5
JOHN DAVID BARRERA ZAPATA <johnbarrera214888@correo.itm.edu.co>
2021-09-03 20:52:28 -0500

    Add Brands

--- Vehicles.API/Data/DataContext.cs
+++ Vehicles.API/Data/DataContext.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Vehicles.API.Data.Entities;
 
 namespace Vehicles.API.Data
@@ -13,11 +9,18 @@ namespace Vehicles.API.Data
         {
         }
 
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Procedure> Procedures { get; set; }
+
         public DbSet<VehicleType> VehicleTypes { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Brand>().HasIndex(x => x.Description).IsUnique();
+            modelBuilder.Entity<Procedure>().HasIndex(x => x.Description).IsUnique();
             modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
         }
 
--- Vehicles.API/Data/Entities/Brand.cs
+++ Vehicles.API/Data/Entities/Brand.cs
@@ -1,12 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Vehicles.API.Data.Entities
 {
-    public class VehicleType
+    public class Brand
     {
         public int Id { get; set; }
 
-        [Display(Name = "Tipo de Vehículo")]
+        [Display(Name = "Marca")]
         [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Description { get; set; }
